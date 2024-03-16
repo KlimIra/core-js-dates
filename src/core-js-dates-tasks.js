9667447325eb310b9d196737daa9fef2a8d1d5fc
Ирина <iraykovleva96@gmail.com>
2024-03-16 13:42:57 +0300

    fix: function getWeekNumberByDate

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -189,9 +189,11 @@ function getCountWeekendsInMonth(month, year) {
  * Date(2024, 1, 23) => 8
  */
 function getWeekNumberByDate(date) {
-  const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
-  const pastDaysOfYear = (date - firstDayOfYear) / 86400000;
-  return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
+  const startOfYear = new Date(date.getFullYear(), 0, 1);
+  const diff = date - startOfYear;
+  const oneDay = 1000 * 60 * 60 * 24;
+  const dayOfYear = Math.floor(diff / oneDay);
+  return Math.ceil((dayOfYear + startOfYear.getDay() + 1) / 7);
 }
 
 /**
