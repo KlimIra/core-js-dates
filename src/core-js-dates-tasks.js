795ea82a5871246ba907e19fab46793e37ca071c
Ирина <iraykovleva96@gmail.com>
2024-03-16 13:37:43 +0300

    fix: function getQuarter

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -221,7 +221,20 @@ function getNextFridayThe13th(/* date */) {
  * Date(2024, 10, 10) => 4
  */
 function getQuarter(date) {
-  return Math.ceil((date.getMonth() + 1) / 3);
+  const month = date.getMonth() + 1;
+
+  switch (true) {
+    case month >= 1 && month <= 3:
+      return 1;
+    case month >= 4 && month <= 6:
+      return 2;
+    case month >= 7 && month <= 9:
+      return 3;
+    case month >= 10 && month <= 12:
+      return 4;
+    default:
+      throw new Error('Invalid month in date provided.');
+  }
 }
 
 /**
